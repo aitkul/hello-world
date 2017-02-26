808b0cb36ebeef45ce77e268415902acd054b714
aitkul <aitkul@gmail.com>
2017-02-26 21:50:49 +0100

    implemented hello world class

--- hello-world/src/main/java/com/aitkul/helloworld/HelloWorld.java
+++ hello-world/src/main/java/com/aitkul/helloworld/HelloWorld.java
@@ -5,5 +5,9 @@ package com.aitkul.helloworld;
  * @author Aitkul
  */
 public class HelloWorld {
-
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
 }
